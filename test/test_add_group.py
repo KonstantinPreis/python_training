383ff1febcc4016f66949aa6f1b9b38f0c2652b6
KonstantinPreis <konstantin.preis@gmail.com>
2016-07-18 18:27:17 +0300

    Created class GroupHelper for wok with groups

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -13,13 +13,13 @@ def app(request):
 
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group("1q2w", "11", "22"))
+    app.group.create(Group("1q2w", "11", "22"))
     app.session.logout()
 
 
 def test_add_empty_group(app):
     app.session.login("admin", "secret")
-    app.create_group(Group("", "", ""))
+    app.group.create(Group("", "", ""))
     app.session.logout()
 
 
