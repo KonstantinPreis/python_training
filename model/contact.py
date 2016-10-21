b14b4c1d38dacc368bb682ab42742b63a7e9824a
KonstantinPreis <konstantin.preis@gmail.com>
2016-10-21 19:28:48 +0300

    create class Contact

--- model/contact.py
+++ model/contact.py
@@ -3,7 +3,7 @@ from sys import maxsize
 
 class Contact:
 
-    def __init__(self, firstname=None, lastname=None, homephone=None, mobilephone=None, \
+    def __init__(self, firstname=None, lastname=None, homephone=None, mobilephone=None,
                  workphone=None, secondaryphone=None, id=None):
         self.firstname = firstname
         self.lastname = lastname
