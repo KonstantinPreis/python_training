5b7c0557c7e76e65fcc9e90c45ecbbd502161bbd
KonstantinPreis <konstantin.preis@gmail.com>
2017-02-07 17:21:19 +0300

    add 6.6
    add 6.6

--- generator/group.py
+++ generator/group.py
@@ -3,7 +3,7 @@ from model.group import Group
 import random
 import string
 import os.path
-import json
+import jsonpickle
 import getopt
 import sys
 
@@ -14,7 +14,7 @@ except getopt.GetoptError as err:
     sys.exit(2)
 
 n = 5
-f = "/data/groups.json"
+f = "data/groups.json"
 
 for o, a in opts:
     if o == "-n":
@@ -36,4 +36,5 @@ testdata = [Group("", "", "")] + [
 file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
 
 with open(file, "w") as f:
-    f.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
+    jsonpickle.set_encoder_options("json", indent=2)
+    f.write(jsonpickle.encode(testdata))
